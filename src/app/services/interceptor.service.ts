a21fdae581f325fd7cba4f26b42db13ab9873380
Boon Seng <soobs0320@gmail.com>
2022-07-11 11:20:40 +0800

    Modify interceptor

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -37,42 +37,59 @@ export class InterceptorService implements HttpInterceptor {
   }
 
   private async handle401(req: HttpRequest<any>, next: HttpHandler) {
-    const refreshTokenFromStorage = await this.storageService.get('refreshToken');
+    console.log('handle 401');
+    const refreshTokenFromStorage = await this.storageService.get(
+      'refreshToken'
+    );
     const refreshToken = await this.erpService
       .getRefreshToken(refreshTokenFromStorage)
       .catch((err) => {
         console.log('Get refresh token error', err);
       });
-    if (refreshToken)
-      await this.storageService.set('refreshToken', refreshToken.data);
 
-    const accessToken = await this.erpService.getAccessToken(refreshToken.data).catch((err) => {
-      console.log('Get access token error', err);
-    });
-    if (accessToken) {
-      await this.storageService.set('token', accessToken.data);
-      req = req.clone({
-        setHeaders: {
-          Authorization: 'Bearer ' + accessToken.data,
-        },
-      });
-      return next
-        .handle(req)
-        .pipe(
-          catchError((err) => {
-            if (err instanceof HttpErrorResponse) {
-              if (err.status == 401) {
-                this.commonService.logout();
-                return throwError(err);
-              }
-            }
-            return throwError(err);
-          })
-        )
-        .toPromise();
-    } else {
+    if (!refreshToken || !refreshToken.data) {
+      console.log('failed to get refresh token');
       this.commonService.logout();
+      return;
     }
+
+    await this.storageService.set('refreshToken', refreshToken.data);
+    const accessToken = await this.erpService
+      .getAccessToken(refreshToken.data)
+      .catch((err) => {
+        console.log('Get access token error', err);
+      });
+
+    if (!accessToken || !accessToken.data) {
+      console.log('failed to get access token');
+      this.commonService.logout();
+      return;
+    }
+
+    await this.storageService.set('token', accessToken.data);
+    req = req.clone({
+      setHeaders: {
+        Authorization: 'Bearer ' + accessToken.data,
+      },
+    });
+
+    console.log('proceed with new tokens');
+    console.log(await this.storageService.get('refreshToken'));
+    console.log(await this.storageService.get('token'));
+    return next
+      .handle(req)
+      .pipe(
+        catchError((err) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status == 401) {
+              this.commonService.logout();
+              return throwError(err);
+            }
+          }
+          return throwError(err);
+        })
+      )
+      .toPromise();
   }
 
   // async handle(req: HttpRequest<any>, next: HttpHandler) {
